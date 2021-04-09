2ee45ae6df4a4d0c5862aaf3b6e2814e79d22400
vagfou <foufoulas.evangelos@protonmail.com>
2021-04-09 15:55:50 +0300

    small cups implemented

--- script.js
+++ script.js
@@ -10,6 +10,11 @@ smallCups.forEach( (cup, idx) => {
 })
 
 function highlightCups(idx) {
+    if (smallCups[idx].classList.contains('full') && 
+    !smallCups[idx].nextElementSibling.classList.contains('full')) {
+        idx--
+    }
+
     smallCups.forEach((cup, idx2) => {
         if (idx2 <= idx) {
             cup.classList.add('full')
